3b10f079c2114b26a851636e2496add23d94b08c
crystal00lim <crystalbb00@gmail.com>
2020-11-05 20:41:14 +0000

    clear canvas

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
   function myCanvas(sw) {
     var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+    var ctx = c.getContext("2d");
 if (sw==0) {
 // Create gradient
 var grd = ctx.createLinearGradient(0,0,200,0);
@@ -10,10 +10,19 @@ grd.addColorStop(1,"red");
 ctx.fillStyle = grd;
 ctx.fillRect(10,10,150,80);
 } 
-else {
-  var grd = ctx.createRadialGradient(75,50,5,90,60,100);
+else if (sw==1) {
+var grd = ctx.createRadialGradient(75,50,5,90,60,100);
 grd.addColorStop(0,"red");
 grd.addColorStop(1,"white");
+
+// Fill with gradient
+ctx.fillStyle = grd;
+ctx.fillRect(10,10,150,80);
+}
+else {
+  var grd = ctx.createRadialGradient(75,50,5,90,60,100);
+grd.addColorStop(0,"transparent");
+grd.addColorStop(1,"transparent");
 
 // Fill with gradient
 ctx.fillStyle = grd;
